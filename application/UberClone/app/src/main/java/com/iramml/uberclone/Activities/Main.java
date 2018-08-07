3248d9f494e23bd1b6541390e8b1415f251fd529
IramML <IramMartinezLamas@gmail.com>
2018-08-07 18:36:04 -0500

    Login

---
+++ application/UberClone/app/src/main/java/com/iramml/uberclone/Activities/Main.java
@@ -0,0 +1,15 @@
+package com.iramml.uberclone.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iramml.uberclone.R;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ application/UberClone/app/src/main/java/com/iramml/uberclone/Model/User.java
@@ -0,0 +1,47 @@
+package com.iramml.uberclone.Model;
+
+public class User{
+    private String email, name, password, phone;
+
+    public User(){
+
+    }
+    public User(String email, String name, String password, String phone) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
